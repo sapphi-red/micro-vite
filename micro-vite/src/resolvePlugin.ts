6f0fb4f0c82f8196b5b6508602e4e59317e29269
sapphi-red <green@sapphi.red>
2022-03-13 19:17:01 +0900

    implement extension fallback

--- micro-vite/src/resolvePlugin.ts
+++ micro-vite/src/resolvePlugin.ts
@@ -4,17 +4,21 @@ import * as fs from 'node:fs/promises'
 
 const root = process.cwd()
 
+const extensions = ['', '.ts', '.js']
+
 export const resolve = (): Plugin => {
   return {
     name: 'micro-vite:resolve',
     async resolveId(id: string) {
-      const absolutePath = path.resolve(root, `.${id}`)
-      try {
-        const stat = await fs.stat(absolutePath)
-        if (stat.isFile()) {
-          return absolutePath
-        }
-      } catch {}
+      for (const ext of extensions) {
+        const absolutePath = path.resolve(root, `.${id}${ext}`)
+        try {
+          const stat = await fs.stat(absolutePath)
+          if (stat.isFile()) {
+            return absolutePath
+          }
+        } catch {}
+      }
       return null
     },
     async load(id: string) {
