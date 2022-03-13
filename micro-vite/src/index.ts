7e74bc5112e55b7cfd971aceaad46a272eb3c39b
sapphi-red <green@sapphi.red>
2022-03-13 17:49:09 +0900

    implement cli

--- micro-vite/src/index.ts
+++ micro-vite/src/index.ts
@@ -1,1 +1,17 @@
-console.log('aa')
+import cac from 'cac'
+
+const cli = cac()
+
+cli.command('dev')
+  .action(() => {
+    console.log('dev server start')
+  })
+
+cli.command('build')
+  .action(() => {
+    console.log('build start')
+  })
+
+cli.help()
+
+cli.parse()
